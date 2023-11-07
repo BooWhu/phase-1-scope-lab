e2205b996e468a29041a87ea1e0451d90f86dc9b
Eli <emmuzio0510@icloud.com>
2023-11-06 20:05:53 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+
+const leastFavoriteCustomer = "someone";
+
+
+function changeLeastFavoriteCustomer() {
+ .
+  leastFavoriteCustomer = "new value";
+}
+
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer,
+};
